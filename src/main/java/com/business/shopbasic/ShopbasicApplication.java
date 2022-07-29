216bd4219053940adc987c67ebb335d4b0f16845
Jagmeet Singh <jagmeet.singh03@sap.com>
2022-07-29 16:40:52 +0530

    adding project files

---
+++ src/main/java/com/business/shopbasic/ShopbasicApplication.java
@@ -0,0 +1,13 @@
+package com.business.shopbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopbasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business/shopbasic/ShopbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.shopbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
